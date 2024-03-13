f034b3e529c051ac92a359a0cdf1710e75fa1786
CyrusLiu20 <kkl20@ic.ac.uk>
2024-03-13 19:45:49 +0000

    Unit test (SolverCG) : real case check

--- test/TestSolverCG.cpp
+++ test/TestSolverCG.cpp
@@ -29,6 +29,25 @@ void PrintMatrix(double* A, int nx, int ny) {
     std::cout << std::endl;
 }
 
+// Function to load data from a file into a vector
+void LoadDataFromFile(const std::string& file_path, double* A, int Npts) {
+    std::ifstream infile(file_path);
+    
+    // Check if the file is opened successfully
+    if (!infile) {
+        std::cerr << "Error: Unable to open the file '" << file_path << "' for reading!" << std::endl;
+        return;
+    }
+
+    // Load data from the file
+    for (int i = 0; i < Npts; i++) {
+        infile >> A[i];
+    }
+
+    // Close the file
+    infile.close();
+}
+
 // Test case for arbitrary vorticity and stream function
 BOOST_AUTO_TEST_CASE(SolveArbitraryCheck)
 {
@@ -44,7 +63,7 @@ BOOST_AUTO_TEST_CASE(SolveArbitraryCheck)
     double* s_true = new double[Npts];
     bool verbose = false;
 
-    // Create LidDrivenCavity environment
+    // Create SolverCg environment
     SolverCG* solver_cg  = new SolverCG(Nx_test,Ny_test,dx_test,dy_test);
 
     for(int i=0;i<Npts;i++){
@@ -56,14 +75,7 @@ BOOST_AUTO_TEST_CASE(SolveArbitraryCheck)
 
     // Open the file for reading
     std::string file_path = "test/data/arbitrary1.txt";
-    std::ifstream infile(file_path);
-
-    // Load the true s stream function matrix
-    for (int i = 0; i < Npts; i++) {
-        infile >> s_true[i];
-    }
-    // Close the file
-    infile.close();
+    LoadDataFromFile(file_path,s_true,Npts);
 
     // Check if solves correctly
     double tolerance = 1e-6;
@@ -82,7 +94,52 @@ BOOST_AUTO_TEST_CASE(SolveArbitraryCheck)
     delete[] s_true;
 }
 
+
 // Test case for real vorticity and stream function
+BOOST_AUTO_TEST_CASE(SolveRealCheck)
+{
+
+    double Lx_test = 2.5;
+    double Ly_test = 1.3;
+    // Grid points for test case
+    int Nx_test = 39;
+    int Ny_test = 47;
+    int Npts = Nx_test*Ny_test;
+
+    double dx_test = Lx_test/(Nx_test+1);
+    double dy_test = Ly_test/(Ny_test+1);
+    double* v = new double[Npts];
+    double* s = new double[Npts];
+    double* v_true = new double[Npts];
+    double* s_true = new double[Npts];
+    bool verbose = false;
+
+    // Open the file for reading
+    std::string file_path = "test/data/realv_input1.txt";
+    LoadDataFromFile(file_path,v,Npts);
+    file_path = "test/data/reals_input1.txt";
+    LoadDataFromFile(file_path,s,Npts);
+    file_path = "test/data/realv_output1.txt";
+    LoadDataFromFile(file_path,v_true,Npts);
+    file_path = "test/data/reals_output1.txt";
+    LoadDataFromFile(file_path,s_true,Npts);
+
+
+    // Create SolverCg environment
+    SolverCG* solver_cg  = new SolverCG(Nx_test,Ny_test,dx_test,dy_test);
+    solver_cg->Solve(v,s,verbose);
+
+    // Check if solves correctly
+    double tolerance = 1e-6;
+    bool all_within = true;
+    for (int i=0;i<Npts;i++){
+        if(std::abs(s[i]-s_true[i])>tolerance){
+            all_within = false;
+        }
+    }
+    BOOST_CHECK_MESSAGE(all_within, "Solved stream function not within tolerance");
+
+}
 
 
 BOOST_AUTO_TEST_SUITE_END()
